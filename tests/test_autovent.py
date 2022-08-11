322c45a4866b6cd593db799ac6b7ec178e43b3f9
Nick Sullivan <nick@sullivanflock.com>
2022-08-11 10:16:11 -0700

    Update tests to use vin-specific end point, use dummy_vin

--- tests/test_autovent.py
+++ tests/test_autovent.py
@@ -8,18 +8,18 @@ from utils import f2c
 
 def test_autovent(requests_mock):
     mock_data = json.loads(open("tests/mock_data/parked.json").read())
-    mock_data["results"][0]["last_state"]["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
-    assert autovent("5YJ3E1EA8KF326283", "dummy_tessie_token", 90, None) == 0, "Initial load should do nothing"
+    mock_data["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) == 0, "Initial load should do nothing"
 
-    mock_data["results"][0]["last_state"]["climate_state"]["inside_temp"] = f2c(95)
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
-    requests_mock.get("https://api.tessie.com/5YJ3E1EA8KF326283/status", text='{"status": "awake"}')
-    requests_mock.get("https://api.tessie.com/5YJ3E1EA8KF326283/command/vent_windows", text='{"result": true }')
-    assert autovent("5YJ3E1EA8KF326283", "dummy_tessie_token", 90, None) == -1, "Windows should roll down"
-    mock_data["results"][0]["last_state"]["vehicle_state"]["rd_window"] = 1
+    mock_data["climate_state"]["inside_temp"] = f2c(95)
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/status", text='{"status": "awake"}')
+    requests_mock.get("https://api.tessie.com/dummy_vin/command/vent_windows", text='{"result": true }')
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) == -1, "Windows should roll down"
+    mock_data["vehicle_state"]["rd_window"] = 1
 
-    mock_data["results"][0]["last_state"]["climate_state"]["inside_temp"] = f2c(85)
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
-    requests_mock.get("https://api.tessie.com/5YJ3E1EA8KF326283/command/close_windows", text='{"result": true }')
-    assert autovent("5YJ3E1EA8KF326283", "dummy_tessie_token", 90, None) == 1, "Windows should roll back up"
+    mock_data["climate_state"]["inside_temp"] = f2c(85)
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/command/close_windows", text='{"result": true }')
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) == 1, "Windows should roll back up"
--- tests/test_peakoff.py
+++ tests/test_peakoff.py
@@ -4,47 +4,47 @@ import arrow
 
 from peakoff import peakoff
 
-TEST_VIN = "5YJ3E1EA8KF326283"
-
 
 def test_peakoff_conditions(requests_mock):
     mock_data = json.loads(open("tests/mock_data/parked.json").read())
-    mock_data["results"][0]["last_state"]["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
+    mock_data["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
 
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     assert (
-        peakoff(TEST_VIN, "dummy_tessie_token", "16:00", "21:00", None) is None
+        peakoff("dummy_vin", "dummy_tessie_token", "16:00", "21:00", None) is None
     ), "Should do nothing while not plugged in"
 
     mock_data = json.loads(open("tests/mock_data/supercharging.json").read())
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     assert (
-        peakoff(TEST_VIN, "dummy_tessie_token", "16:00", "21:00", None) is None
+        peakoff("dummy_vin", "dummy_tessie_token", "16:00", "21:00", None) is None
     ), "Should do nothing while supercharging"
 
 
 def test_peakoff_toggling(requests_mock):
     mock_data = json.loads(open("tests/mock_data/supercharging.json").read())
-    mock_data["results"][0]["last_state"]["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
-    mock_data["results"][0]["last_state"]["charge_state"]["charger_voltage"] = 100
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
+    mock_data["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
+    mock_data["charge_state"]["charger_voltage"] = 100
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
 
-    assert peakoff(TEST_VIN, "dummy_tessie_token", "00:00", "00:00", None) == 0, "Should leave it alone outside of peak"
+    assert (
+        peakoff("dummy_vin", "dummy_tessie_token", "00:00", "00:00", None) == 0
+    ), "Should leave it alone outside of peak"
 
-    requests_mock.get(f"https://api.tessie.com/{TEST_VIN}/command/stop_charging", text='{"result": true }')
-    assert peakoff(TEST_VIN, "dummy_tessie_token", "00:00", "23:59", None) == -1, "Should stop charging during peak"
+    requests_mock.get("https://api.tessie.com/dummy_vin/command/stop_charging", text='{"result": true }')
+    assert peakoff("dummy_vin", "dummy_tessie_token", "00:00", "23:59", None) == -1, "Should stop charging during peak"
 
-    mock_data["results"][0]["last_state"]["charge_state"]["charging_state"] = "Stopped"
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
-    requests_mock.get(f"https://api.tessie.com/{TEST_VIN}/command/start_charging", text='{"result": true }')
-    assert peakoff(TEST_VIN, "dummy_tessie_token", "00:00", "00:00", None) == 1, "Should resume charging off peak"
+    mock_data["charge_state"]["charging_state"] = "Stopped"
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/command/start_charging", text='{"result": true }')
+    assert peakoff("dummy_vin", "dummy_tessie_token", "00:00", "00:00", None) == 1, "Should resume charging off peak"
 
 
 def test_low_battery(requests_mock):
     mock_data = json.loads(open("tests/mock_data/residential_charging.json").read())
-    mock_data["results"][0]["last_state"]["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
+    mock_data["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
 
-    requests_mock.get("https://api.tessie.com/vehicles", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     assert (
-        peakoff(TEST_VIN, "dummy_tessie_token", "00:00", "23:59", None) == 0
+        peakoff("dummy_vin", "dummy_tessie_token", "00:00", "23:59", None) == 0
     ), "Should leave it charging because the battery is too low to optimize"

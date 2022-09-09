4d137be26b3d019f7f47e9ee03790f4ca9bfbb28
Nick Sullivan <nick@sullivanflock.com>
2022-09-08 21:30:15 -0700

    Additional tests for auto vent

--- tests/test_autovent.py
+++ tests/test_autovent.py
@@ -3,6 +3,22 @@ from utils import f2c
 import arrow, json
 
 
+def test_stale_data(requests_mock):
+
+    mock_data = json.loads(open("tests/mock_data/parked.json").read())
+
+    # Stale data
+    mock_data["drive_state"]["timestamp"] = arrow.utcnow().shift(hours=-12).timestamp() * 1000
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) is None, "Should leave the car sleeping at night"
+
+    mock_data["drive_state"]["timestamp"] = arrow.utcnow().shift(hours=-5).timestamp() * 1000
+    requests_mock.get("https://api.tessie.com/dummy_vin/status", text='{"status": "asleep"}')
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    requests_mock.get("https://api.tessie.com/dummy_vin/wake", text='{"result": true }')
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) is None, "Should wake up the car during the day"
+
+
 def test_autovent(requests_mock):
 
     mock_data = json.loads(open("tests/mock_data/parked.json").read())
@@ -10,6 +26,13 @@ def test_autovent(requests_mock):
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     assert autovent("dummy_vin", "dummy_tessie_token", 90, None) == 0, "Initial load should do nothing"
 
+    # Conditions
+    mock_data["drive_state"]["speed"] = 10
+    requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) is None, "Should not vent if the car is moving"
+    mock_data["drive_state"]["speed"] = None
+
+    # Let's make it hot
     mock_data["climate_state"]["inside_temp"] = f2c(95)
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     requests_mock.get("https://api.tessie.com/dummy_vin/status", text='{"status": "awake"}')
@@ -17,6 +40,7 @@ def test_autovent(requests_mock):
     assert autovent("dummy_vin", "dummy_tessie_token", 90, None) == -1, "Windows should roll down"
     mock_data["vehicle_state"]["rd_window"] = 1
 
+    # Let's cool it down
     mock_data["climate_state"]["inside_temp"] = f2c(85)
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     requests_mock.get("https://api.tessie.com/dummy_vin/command/close_windows", text='{"result": true }')

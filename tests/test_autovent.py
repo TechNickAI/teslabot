665a393b9f08c2b0dde31b993272d9a2e4c127d0
Nick Sullivan <nick@sullivanflock.com>
2022-09-08 21:51:20 -0700

    More selective about notify_phone testing

--- tests/test_autovent.py
+++ tests/test_autovent.py
@@ -7,22 +7,16 @@ def test_stale_data(requests_mock):
 
     mock_data = json.loads(open("tests/mock_data/parked.json").read())
 
-    requests_mock.post("https://api.twilio.com/2010-04-01/Accounts/ACXXX/Messages.json", text='{"sid": "SMXXX"}')
-
     # Stale data
     mock_data["drive_state"]["timestamp"] = arrow.utcnow().shift(hours=-12).timestamp() * 1000
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
-    assert (
-        autovent("dummy_vin", "dummy_tessie_token", 90, "+18005551212") is None
-    ), "Should leave the car sleeping at night"
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) is None, "Should leave the car sleeping at night"
 
     mock_data["drive_state"]["timestamp"] = arrow.utcnow().shift(hours=-5).timestamp() * 1000
     requests_mock.get("https://api.tessie.com/dummy_vin/status", text='{"status": "asleep"}')
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     requests_mock.get("https://api.tessie.com/dummy_vin/wake", text='{"result": true }')
-    assert (
-        autovent("dummy_vin", "dummy_tessie_token", 90, "+18005551212") is None
-    ), "Should wake up the car during the day"
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) is None, "Should wake up the car during the day"
 
 
 def test_autovent(requests_mock):
@@ -30,19 +24,17 @@ def test_autovent(requests_mock):
     mock_data = json.loads(open("tests/mock_data/parked.json").read())
     mock_data["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
 
-    requests_mock.post("https://api.twilio.com/2010-04-01/Accounts/ACXXX/Messages.json", text='{"sid": "SMXXX"}')
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
-    assert autovent("dummy_vin", "dummy_tessie_token", 90, "+18005551212") == 0, "Initial load should do nothing"
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) == 0, "Initial load should do nothing"
 
     # Conditions
     mock_data["drive_state"]["speed"] = 10
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
-    assert (
-        autovent("dummy_vin", "dummy_tessie_token", 90, "+18005551212") is None
-    ), "Should not vent if the car is moving"
+    assert autovent("dummy_vin", "dummy_tessie_token", 90, None) is None, "Should not vent if the car is moving"
     mock_data["drive_state"]["speed"] = None
 
     # Let's make it hot
+    requests_mock.post("https://api.twilio.com/2010-04-01/Accounts/ACXXX/Messages.json", text='{"sid": "SMXXX"}')
     mock_data["climate_state"]["inside_temp"] = f2c(95)
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     requests_mock.get("https://api.tessie.com/dummy_vin/status", text='{"status": "awake"}')
--- tests/test_peakoff.py
+++ tests/test_peakoff.py
@@ -32,21 +32,24 @@ def test_peakoff_toggling(requests_mock):
     ), "Should leave it alone outside of peak"
 
     requests_mock.get("https://api.tessie.com/dummy_vin/command/stop_charging", text='{"result": true }')
-    assert peakoff("dummy_vin", "dummy_tessie_token", "00:00", "23:59", None) == -1, "Should stop charging during peak"
+    assert (
+        peakoff("dummy_vin", "dummy_tessie_token", "00:00", "23:59", "+18005551212") == -1
+    ), "Should stop charging during peak"
 
     mock_data["charge_state"]["charging_state"] = "Stopped"
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
     assert peakoff("dummy_vin", "dummy_tessie_token", "00:00", "23:59", None) == 0, "Should leave as is during peak"
 
     requests_mock.get("https://api.tessie.com/dummy_vin/command/start_charging", text='{"result": true }')
-    assert peakoff("dummy_vin", "dummy_tessie_token", "00:00", "00:00", None) == 1, "Should resume charging off peak"
+    assert (
+        peakoff("dummy_vin", "dummy_tessie_token", "00:00", "00:00", "+18005551212") == 1
+    ), "Should resume charging off peak"
 
 
 def test_low_battery(requests_mock):
 
     mock_data = json.loads(open("tests/mock_data/residential_charging.json").read())
     mock_data["drive_state"]["timestamp"] = arrow.utcnow().timestamp() * 1000
-    requests_mock.post("https://api.twilio.com/2010-04-01/Accounts/ACXXX/Messages.json", text='{"sid": "SMXXX"}')
 
     mock_data["charge_state"]["battery_level"] = 10
     requests_mock.get("https://api.tessie.com/dummy_vin/state", text=json.dumps(mock_data))
